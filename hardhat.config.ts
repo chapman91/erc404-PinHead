b05336b7e05a85cb6787d7ab00d04041ac1b6b03
chapman91 <chapmandoneil91@gmail.com>
2024-04-07 00:23:47 -0400

    smart contract set up

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.HTTP_PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  gasReporter: { enabled: true },
+};
+
+export default config;
